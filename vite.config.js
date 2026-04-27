3f6fb5f83baeff43d108d65b5eec7b4f3d6f31a7
hmzaryan1-design <hmza.ryan1@gmail.com>
2026-04-28 00:44:40 +0100

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        services: resolve(__dirname, 'services.html'),
+        caseStudies: resolve(__dirname, 'case-studies.html'),
+        about: resolve(__dirname, 'about.html'),
+        contact: resolve(__dirname, 'contact.html'),
+      },
+    },
+  },
+});
